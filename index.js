ce372ba2cd5927226740056051aabd44d74e46e1
Agnivesh  Pandita <105123072+Agni133@users.noreply.github.com>
2024-05-20 03:26:18 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+import inquirer from 'inquirer';
+ import qr from "qr-image";
+ import fs, { writeFile } from "fs";
+import { log } from 'console';
+
+inquirer
+  .prompt([
+    { message:"type in ur url:",
+    
+    name:"URL"
+
+     }
+  ])
+  .then((answers) => {
+    const url= answers.URL;
+    var qr_svg = qr.image(url);
+qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+   
+fs.writeFile("URL.txt",url,(err)=>{
+    if(err)throw err;
+    console.log("The file has been saved!");
+});
+
+
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
