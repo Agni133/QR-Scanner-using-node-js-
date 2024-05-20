2bd64652c2e6d885c5ed585be3c3f0a76fb50828
Agnivesh  Pandita <105123072+Agni133@users.noreply.github.com>
2024-05-21 02:45:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,8 +25,7 @@ fs.writeFile("URL.txt",url,(err)=>{
   })
   .catch((error) => {
     if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
     } else {
-      // Something else went wrong
+    
     }
   });
